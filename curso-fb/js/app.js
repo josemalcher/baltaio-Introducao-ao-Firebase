94cfafa2da2d94d34627df07a7e99ae891da1790
José Malcher Jr <contato@josemalcher.net>
2021-01-07 20:35:36 -0300

    Aula 9 - Real-Time Database

---
+++ curso-fb/js/app.js
@@ -0,0 +1,14 @@
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+var firebaseConfig = {
+    apiKey: "AIzaSyCwQMeN1ZnBID2cFsOctUBqzFvlREHSsYI",
+    authDomain: "curso-fb-josemalcher.firebaseapp.com",
+    projectId: "curso-fb-josemalcher",
+    storageBucket: "curso-fb-josemalcher.appspot.com",
+    messagingSenderId: "180355740126",
+    appId: "1:180355740126:web:6e4ce2786f386a5e066d4f",
+    measurementId: "G-H5VR1PQ2M0"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
